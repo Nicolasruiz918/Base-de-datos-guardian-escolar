d8aecc4693c002a6ba6c337c960f36a9491eb7da
Nicolas Ruiz <nruizsastoque@gmail.com>
2026-09-06 22:16:20 -0500

    new structure

--- 05_rollbacks/01_ddl/01_schemas/001_public_schema.rollback.sql
+++
@@ -1,1 +0,0 @@
-COMMENT ON SCHEMA public IS NULL;
--- 05_rollbacks/01_ddl/04_views/001_domain_views.rollback.sql
+++
@@ -1,3 +0,0 @@
-DROP VIEW IF EXISTS vw_notificaciones_usuario;
-DROP VIEW IF EXISTS vw_trayectos_resumen;
-DROP VIEW IF EXISTS vw_estudiantes_rutas;
